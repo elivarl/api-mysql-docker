44f9a337f8371799a0ef77dbccf651fb4bb259df
elargo <elargor@gmail.com>
2025-03-25 08:29:25 -0500

    Creacion proyecto inicial

---
+++ src/main/java/com/icodeap/apimysqldocker/ApimysqldockerApplication.java
@@ -0,0 +1,13 @@
+package com.icodeap.apimysqldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimysqldockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimysqldockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icodeap/apimysqldocker/ApimysqldockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap.apimysqldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimysqldockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
